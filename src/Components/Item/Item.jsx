a9a76618e55ccd696de689f3b52ac03f94535314
Umesh7Dixit <umesh7dixit@gmail.com>
2024-06-16 09:29:19 +0530

    adding discount % section and border radius

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -5,13 +5,16 @@ import { ShopContext } from '../../Context/ShopContext'
 
 const Item = (props) => {
     const {theme}=useContext(ShopContext);
+    const discount = (props.old_price)-(props.new_price);
+    const discount_per = ((discount/props.old_price)*100).toFixed(0);
     return (
         <div className='item'>
             <Link to={`/product/${props.id}`}> <img onClick={window.scrollTo(0,0)} src={props.image} alt="" /></Link>
             <p className={`pit_${theme}`}>{props.name}</p><p className={`pit_${theme}`}>{props.id}</p>
             <div className="item-prices">
                 <div className={`item-price-new_${theme}`}>${props.new_price}</div>
-                <div className={`item-price-old_${theme}`}>${props.old_price}</div>
+                <div className={`item-price-old_${theme}`}>${props.old_price}</div>             
+                <div style={{color:"red"}}>{`${discount_per}% off`}</div>
             </div>
         </div>
     )
